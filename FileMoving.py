ee89710fb24ca90cd4f1c7b3363d5ff4d328001e
joe2475 <33675219+joe2475@users.noreply.github.com>
2020-04-08 10:37:48 -0500

    Update FileMoving.py

--- FileMoving.py
+++ FileMoving.py
@@ -6,7 +6,7 @@ from os import path
 
 def dirCheckMove(src,dest,dirname,filename, subfolder = False, subOne = '', subTwo = ''):
     count = 0
-    isdir01_ACCTG01 = os.path.isdir(src + dirname)
+    dirCheck = os.path.isdir(src + dirname)
     if (isdir01_ACCTG01 == True):
         files = os.listdir(src + dirname)
         for f in files:
